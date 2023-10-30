b28c25711c189892890ad09cd40da841fe3deef1
raistatement <145664649+raistatement@users.noreply.github.com>
2023-10-30 14:44:21 +0000

    Group K Presentation

---
+++ my-app/src/routes/+page.svelte
@@ -0,0 +1,163 @@
+<svelte:head>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+</svelte:head>
+          
+
+
+
+<script lang="ts">
+
+    import slide1 from '/workspaces/SvelteTS23/my-app/src/images/slide1.jpg';
+    import slide2 from '/workspaces/SvelteTS23/my-app/src/images/slide2.jpg';
+    import slide3 from '/workspaces/SvelteTS23/my-app/src/images/slide3.jpg';
+    import slide4 from '/workspaces/SvelteTS23/my-app/src/images/slide4.jpg';
+    import slide5 from '/workspaces/SvelteTS23/my-app/src/images/slide5.jpg';
+    import slide6 from '/workspaces/SvelteTS23/my-app/src/images/slide6.jpg';
+    import slide7 from '/workspaces/SvelteTS23/my-app/src/images/slide7.jpg';
+    import slide8 from '/workspaces/SvelteTS23/my-app/src/images/slide8.jpg';
+    import slide9 from '/workspaces/SvelteTS23/my-app/src/images/slide9.jpg';
+    import slide10 from '/workspaces/SvelteTS23/my-app/src/images/slide10.jpg';
+    import slide11 from '/workspaces/SvelteTS23/my-app/src/images/slide11.jpg';
+    import slide12 from '/workspaces/SvelteTS23/my-app/src/images/slide12.jpg';
+    import slide13 from '/workspaces/SvelteTS23/my-app/src/images/slide13.jpg';
+    import slide14 from '/workspaces/SvelteTS23/my-app/src/images/slide14.jpg';
+    import slide15 from '/workspaces/SvelteTS23/my-app/src/images/slide15.jpg';
+    import slide16 from '/workspaces/SvelteTS23/my-app/src/images/slide16.jpg';
+    import slide17 from '/workspaces/SvelteTS23/my-app/src/images/slide17.jpg';
+    import slide18 from '/workspaces/SvelteTS23/my-app/src/images/slide18.jpg';
+    import slide19 from '/workspaces/SvelteTS23/my-app/src/images/slide19.jpg';
+    import slide20 from '/workspaces/SvelteTS23/my-app/src/images/slide20.jpg';
+    import slide21 from '/workspaces/SvelteTS23/my-app/src/images/slide21.jpg';
+    import slide22 from '/workspaces/SvelteTS23/my-app/src/images/slide22.jpg';
+    import slide23 from '/workspaces/SvelteTS23/my-app/src/images/slide23.jpg';
+    import slide24 from '/workspaces/SvelteTS23/my-app/src/images/slide24.jpg';
+    import slide25 from '/workspaces/SvelteTS23/my-app/src/images/slide25.jpg';
+    import slide26 from '/workspaces/SvelteTS23/my-app/src/images/slide26.jpg';
+    import slide27 from '/workspaces/SvelteTS23/my-app/src/images/slide27.jpg';
+    import slide28 from '/workspaces/SvelteTS23/my-app/src/images/slide28.jpg';
+    import slide29 from '/workspaces/SvelteTS23/my-app/src/images/slide29.jpg';
+    import slide30 from '/workspaces/SvelteTS23/my-app/src/images/slide30.jpg';
+    import slide31 from '/workspaces/SvelteTS23/my-app/src/images/slide31.jpg';
+  </script>
+ 
+
+  <style>
+ 
+  .custom-image {
+    max-width: 100%;
+    max-height: 100%;
+    display: block;
+    margin: 0 auto;
+    border-radius: 10px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  }
+
+  .slideheading {
+    text-align: center;
+    margin-bottom: 20px;
+  }
+
+  
+</style>
+
+  
+  <div class="slideheading">
+    <h1>Oracle Cloud</h1>
+    <img src="{slide1}" alt="Slide 1" class="custom-image">
+
+    <h1>Contents</h1>
+    <img src="{slide2}" alt="Slide 2" class="custom-image">
+
+    <h1>Introduction</h1>
+    <img src="{slide3}" alt="Slide 3" class="custom-image">
+    
+    <h1>Introduction 1 </h1>
+    <img src="{slide4}" alt="Slide 4" class="custom-image">
+
+    <h1>Introduction 2</h1>
+    <img src="{slide5}" alt="Slide 5" class="custom-image">
+
+    <h1>Technical Elements</h1>
+    <img src="{slide6}" alt="Slide 6" class="custom-image">
+    
+    <h1>Technical Elements and Interaction</h1>
+    <img src="{slide7}" alt="Slide 7" class="custom-image">
+
+    <h1>How to create a Autonomous Database</h1>
+    <img src="{slide8}" alt="Slide 8" class="custom-image">
+
+    <h1>Choosing Workload/Development Type of Database</h1>
+    <img src="{slide9}" alt="Slide 9" class="custom-image">
+
+    <h1>Configuring Database</h1>
+    <img src="{slide10}" alt="Slide 10" class="custom-image">
+
+    <h1>Creating Admin Crendentials</h1>
+    <img src="{slide11}" alt="Slide 11" class="custom-image">
+
+    <h1>Choose network access</h1>
+    <img src="{slide12}" alt="Slide 12" class="custom-image">
+    
+    <h1>Providing Contacts for Operational Notifications</h1>
+    <img src="{slide13}" alt="Slide 13" class="custom-image">
+
+    <h1>Information on Newly Created Database</h1>
+    <img src="{slide14}" alt="Slide 14" class="custom-image">
+
+    <h1>Input of Information into Database</h1>
+    <img src="{slide15}" alt="Slide 15" class="custom-image">
+
+    <h1>Showing of Primary and Foreign Keys</h1>
+    <img src="{slide16}" alt="Slide 16" class="custom-image">
+
+    <h1>Information Input to Database</h1>
+    <img src="{slide17}" alt="Slide 17" class="custom-image">
+
+    <h1>Use of Oracle SQL Developer</h1>
+    <img src="{slide18}" alt="Slide 18" class="custom-image">
+
+    <h1>Oracle Explorer</h1>
+    <img src="{slide19}" alt="Slide 19" class="custom-image">
+
+    <h1>Technical Elements and How They Collaborate with Other Components</h1>
+    <img src="{slide20}" alt="Slide 20" class="custom-image">
+
+    <h1>Creating of Table using SQL</h1>
+    <img src="{slide21}" alt="Slide 21" class="custom-image">
+
+    <h1>Connecting Remotely to Autonomous Database</h1>
+    <img src="{slide22}" alt="Slide 22" class="custom-image">
+
+    <h1>Simple HTML to display contents of database</h1>
+    <img src="{slide23}" alt="Slide 23" class="custom-image">
+
+    <h1>Javascript is used to read and display contents of database</h1>
+    <img src="{slide24}" alt="Slide 24" class="custom-image">
+
+    <h1>Results Displayed on Page</h1>
+    <img src="{slide25}" alt="Slide 25" class="custom-image">
+
+    <h1>Usage Scenarios</h1>
+    <img src="{slide26}" alt="Slide 26" class="custom-image">
+
+    <h1>Usage Scenarios 1</h1>
+    <img src="{slide27}" alt="Slide 27" class="custom-image">
+
+    <h1>Pros and Cons</h1>
+    <img src="{slide28}" alt="Slide 28" class="custom-image">
+
+    <h1>Pros and Cons 1</h1>
+    <img src="{slide29}" alt="Slide 29" class="custom-image">
+
+    <h1>Pros and Cons 2</h1>
+    <img src="{slide30}" alt="Slide 30" class="custom-image">
+
+    <h1>End of Presentation</h1>
+    <img src="{slide31}" alt="Slide 31" class="custom-image">
+
+
+</div>
+
+   
+
+  
